c081a26ba057025095ec6c38037a1d7667b327a0
1010Rohit <parmarrohit.rp43@gmail.com>
2025-04-11 12:53:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+function removal() {
+	var dele =document.getElementById("colorSelect");
+	var selele= dele.selectedIndex;
+	if(selele !== -1){
+		dele.remove(selele)
+	}
+}
